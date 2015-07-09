72bc41d931d84d740e4073a52de7e3e0413599f8
Ladislav Maxa <ladislavmaxa@outlook.com>
2015-07-10 01:57:09 +0200

    update scaling

--- device.js
+++ device.js
@@ -4,23 +4,29 @@ var scale = 1;
 
 //iPhone4
 if (device.width === 320 && device.height == 480) {
-    scale = 0.592;
+    scale = 0.77;
 }
 
 //iPhone5
 if (device.width === 320 && device.height == 568) {
-    scale = 0.592;
+    scale = 0.77;
 }
 
 //iPhone6
 if (device.width === 375 && device.height == 667) {
-    scale = 0.6944;
+    scale = 0.85;
 }
 
 //iPhone6plus
 if (device.width === 414 && device.height == 736) {
     scale = 1;
 }
+
+
+//iPad
+//if (device.width === 768 && device.height == 1024) {
+//    scale = 1;
+//}
 
 
 module.exports = {
