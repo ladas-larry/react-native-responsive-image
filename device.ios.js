023c10582570bcac09388484de144a4e6bd438da
Ladislav Maxa <ladislavmaxa@outlook.com>
2016-01-03 18:50:54 +0100

    iOS polishing

---
+++ device.ios.js
@@ -0,0 +1,25 @@
+var React = require('react-native');
+var { Dimensions } = React;
+
+var device = Dimensions.get('window');
+var scale;
+
+switch (device.width) {
+	//iPhone4/4S and iPhone5/5S
+  		case 320:
+		scale = 0.77;
+		break;
+	//iPhone6/6S
+  		case 375:
+		scale = 0.902;
+		break;
+	//iPhone6plus/6Splus
+  		case 414:
+		scale = 1;
+		break;
+	//iPad
+	default:
+		scale = 1;
+}
+
+module.exports = { scale: scale };
