efb490e10a0a298b595b51306b1c5ee48f76e776
Ladislav Maxa <ladislavmaxa@outlook.com>
2017-01-11 11:20:32 +0100

    es2015 syntax

--- react-native-responsive-image.js
+++ react-native-responsive-image.js
@@ -1,12 +1,14 @@
-var React = require('react');
-var applyScale = require('./applyScale');
-var { Image } = require('react-native');
+module.exports = ResponsiveImage;
 
-var ResponsiveImage = React.createClass({
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import applyScale from './applyScale';
+export default class ResponsiveImage extends Component {
+    /*
     setNativeProps: function(nativeProps) {
         this._root.setNativeProps(nativeProps);
-    },
-    render: function () {
+    },*/
+    render(){
         var width = applyScale(this.props.initWidth);
         var height = applyScale(this.props.initHeight);
         return (
@@ -18,6 +20,5 @@ var ResponsiveImage = React.createClass({
             </Image>
         );
     }
-});
+}
 
-module.exports = ResponsiveImage;
