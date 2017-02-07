e50c2f1f4e8d5a32509c21ba3c450d07e37bdcaf
Ladislav Maxa <ladislavmaxa@outlook.com>
2017-02-07 03:06:22 +0100

    fix setNativeProps

--- react-native-responsive-image.js
+++ react-native-responsive-image.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Image } from 'react-native';
 import applyScale from './applyScale';
 export default class ResponsiveImage extends Component {
-    /*
-    setNativeProps: function(nativeProps) {
+
+    setNativeProps(nativeProps) {
         this._root.setNativeProps(nativeProps);
-    },*/
+    }
+    
     render(){
         var width = applyScale(this.props.initWidth);
         var height = applyScale(this.props.initHeight);
